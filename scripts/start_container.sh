61415b80f8a5a61770cab89feac9df789c4321b7
Bibek Aryal <125453535+yesBibekaryal@users.noreply.github.com>
2025-07-09 23:10:12 +1000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull kubebibek/bankapp:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 kubebibek/bankapp:latest
